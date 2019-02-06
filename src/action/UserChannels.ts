0d7bedb3b8d39ca7b0781e6493bccab54eeb674a
oleast <oleast@stud.ntnu.no>
2019-02-06 16:08:55 +0100

    Add actions for manipulating user channels

---
+++ src/action/UserChannels.ts
@@ -0,0 +1,15 @@
+import * as UserController from '../controller/UserController';
+import { IUserContext } from '../middlewares';
+
+export async function postUserChannels(context: IUserContext) {
+  const { username } = context.user;
+  const { channels: channelNames } = context.body;
+  const channels = await UserController.setChannels(username, channelNames);
+  context.body = channels;
+}
+
+export async function getUserChannels(context: IUserContext) {
+  const { username } = context.user;
+  const channels = await UserController.getChannels(username);
+  context.body = channels;
+}
